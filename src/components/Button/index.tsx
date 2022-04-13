ede9658f5eb89cab0a9545def9797b5ab6fe3622
chon26909 <59905927+chon26909@users.noreply.github.com>
2022-04-13 14:52:20 +0700

    custom button

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,14 @@
+import React, { ButtonHTMLAttributes } from 'react';
+import styles from './index.module.scss';
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> { };
+
+const index: React.FC<ButtonProps> = ({children}) => {
+  return (
+    <button className={styles.Button}>
+        {children}
+    </button>
+  )
+}
+
+export default index;
