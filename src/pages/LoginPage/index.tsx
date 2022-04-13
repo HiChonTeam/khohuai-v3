3965bbb4607ea52aaf10cd2faab6b9c556bed7c1
chon26909 <59905927+chon26909@users.noreply.github.com>
2022-04-13 14:52:00 +0700

    spinner gif

---
+++ src/pages/LoginPage/index.tsx
@@ -0,0 +1,21 @@
+import React, { useRef } from "react";
+import styles from "./index.module.scss";
+import Input from "../../components/Input";
+import Button from "../../components/Button";
+
+const Index: React.FC = () => {
+
+  const email = useRef(null);
+  const password = useRef(null);
+
+  return (
+    <div className={styles.LoginPage}>
+      <div className={styles.Title}>เข้าสู่ระบบ</div>
+      <Input type="text" label="อีเมล" ref={email} />
+      <Input type="password" label="รหัสผ่าน" ref={password} />
+      <Button type="button">ลงชื่อเข้าใช้</Button>
+    </div>
+  );
+};
+
+export default Index;
