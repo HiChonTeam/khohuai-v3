6cb32b541d320d5f6c29b64ab68fb9e7011a7635
chon26909 <59905927+chon26909@users.noreply.github.com>
2022-04-13 14:55:18 +0700

    main page

---
+++ src/pages/Mainpage/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import HotNumber from './HotNumber';
+import styles from './index.module.scss';
+import LandingScreen from './LandingScreen';
+
+
+const Mainpage: React.FC = () => {
+  return (
+    <div className={styles.background} id="#">
+      <LandingScreen/>
+      <HotNumber/>
+    </div>
+  )
+}
+
+export default Mainpage
