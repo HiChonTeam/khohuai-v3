eb2f88355fd8f3d4c1571554ebf595aba5b293e7
chon26909 <59905927+chon26909@users.noreply.github.com>
2022-04-13 14:53:26 +0700

    spinner component

---
+++ src/components/Loading/Spinner.tsx
@@ -0,0 +1,36 @@
+import './Spinner.scss';
+
+const Spinner = () => {
+  return (
+    <div className="loadingio-spinner-spin-9qm07ls7maa">
+      <div className="ldio-uairjl9wo88">
+        <div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Spinner;
