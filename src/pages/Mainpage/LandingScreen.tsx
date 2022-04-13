74e3b23a5d33fe2c687f234edc0e801b124de92f
chon26909 <59905927+chon26909@users.noreply.github.com>
2022-04-13 14:55:03 +0700

    landing page

---
+++ src/pages/Mainpage/LandingScreen.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import styles from "./LandingScreen.module.scss";
+import background from "../../assets/images/main_background.jpg";
+import { Link } from "react-router-dom";
+
+const LandingScreen: React.FC = () => {
+  return (
+    <div className={styles.Landing}>
+      <img src={background} className={styles.background} alt="background" />
+      <div className={styles.overlay}>
+        <div className={["container", styles.grid].join(" ")}>
+          <div>
+            <h2>Online Platform</h2>
+            <h3>จำหน่ายสลากออนไลน์</h3>
+            <h4>ซื้อง่าย ซื้อได้ทุกที่ ทุกเวลา</h4>
+           <Link to={'/login'} className={styles.btnBuynow}>
+              ซื้อเลย
+           </Link> 
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LandingScreen;
