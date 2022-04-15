fef59e2e35bd49221a7da6d25ae50502fed0e4b9
chon26909 <59905927+chon26909@users.noreply.github.com>
2022-04-15 11:53:33 +0700

    edit route

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,26 @@
 import React from 'react';
 
+interface routeType {
+  path: string,
+  element: any;
+}
+
+interface routerType {
+  [key: string]: routeType[]
+};
+
 //pages
 const MainPage = React.lazy(() => import('./pages/Mainpage'));
+const LoginPage = React.lazy(() => import("./pages/LoginPage"));
 
 
-const routes = [
-  { path: '/', element: MainPage },
-]
+const routes: routerType = {
+  notlogin: [
+    { path: '/login', element: LoginPage },
+  ],
+  main: [
+    { path: '/', element: MainPage },
+  ]
+}
 
 export default routes;
