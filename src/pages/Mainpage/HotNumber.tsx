e830086d7cfd7232e50f3345b65c997f461c07b0
chon26909 <59905927+chon26909@users.noreply.github.com>
2022-04-15 11:54:01 +0700

    update hot number

--- src/pages/Mainpage/HotNumber.tsx
+++ src/pages/Mainpage/HotNumber.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import { useAppDispatch, useAppSelector } from '../../hook';
+import bestseller from '../../store/slices/bestseller';
 import styles from './HotNumber.module.scss';
 
+
 const HotNumber = () => {
+
+  const dispatch = useAppDispatch();
+  const baseSeller = useAppSelector((state) => state.bestSeller);
+
+
   return (
-    <div className={['container'].join(' ')} style={{height: '100vh'}}>HotNumber</div>
+    <div className={['container', styles.HotNumber].join(' ')} >
+      <h2>กำลังหมด</h2>
+      {
+        baseSeller.data.map((item, index) => { 
+          return (
+            <div key={index}>
+              {
+                item.number
+              }
+            </div>
+          )
+        })
+      }
+    </div>
   )
 }
 
