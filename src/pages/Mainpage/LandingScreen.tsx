d91ba9fe78c0d08b03f323e694ea7e445cfe5f05
chon26909 <59905927+chon26909@users.noreply.github.com>
2022-04-15 11:53:48 +0700

    update landing page

--- src/pages/Mainpage/LandingScreen.tsx
+++ src/pages/Mainpage/LandingScreen.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./LandingScreen.module.scss";
 import background from "../../assets/images/main_background.jpg";
+import Button from '../../components/Button';
 import { Link } from "react-router-dom";
 
 const LandingScreen: React.FC = () => {
@@ -13,8 +14,8 @@ const LandingScreen: React.FC = () => {
             <h2>Online Platform</h2>
             <h3>จำหน่ายสลากออนไลน์</h3>
             <h4>ซื้อง่าย ซื้อได้ทุกที่ ทุกเวลา</h4>
-           <Link to={'/login'} className={styles.btnBuynow}>
-              ซื้อเลย
+           <Link to={'/login'}>
+              <Button color="primary" >ซื้อเลย</Button>
            </Link> 
           </div>
         </div>
