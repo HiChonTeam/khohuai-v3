479b4609c6d1159f101e5a9c8293591199882d9e
chon26909 <59905927+chon26909@users.noreply.github.com>
2022-04-15 11:55:50 +0700

    custom button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,14 +1,28 @@
-import React, { ButtonHTMLAttributes } from 'react';
-import styles from './index.module.scss';
+import React, { ButtonHTMLAttributes, FC, ForwardRefRenderFunction } from "react";
+import styles from "./index.module.scss";
+import Ripples from "react-ripples";
 
-interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> { };
-
-const index: React.FC<ButtonProps> = ({children}) => {
-  return (
-    <button className={styles.Button}> 
-        {children}
-    </button>
-  )
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  className?: string;
+  color?: string;
+  full?: boolean;
 }
 
-export default index;
+const Index:FC <ButtonProps> = ({ children, className, color, full = false }) => {
+  return (
+    <Ripples className={styles.RippleButton} color="#f1f1f16e" during={700}>
+      <button
+        className={[
+          className,
+          styles.Button,
+          (color === "primary" ? styles.Color : " "),
+          (full ? styles.Full : '' )
+        ].join(" ")}
+      >
+        {children}
+      </button>
+    </Ripples>
+  );
+};
+
+export default Index;
