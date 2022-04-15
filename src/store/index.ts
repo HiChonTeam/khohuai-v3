9fd4f902596843cda1b8bdebea3bacb4f259b425
chon26909 <59905927+chon26909@users.noreply.github.com>
2022-04-15 11:53:17 +0700

    edit store

---
+++ src/store/index.ts
@@ -0,0 +1,18 @@
+import { configureStore } from '@reduxjs/toolkit'
+// import navbarReducer from './slices/navbar';
+import bestseller from './slices/bestseller';
+
+const reducer = {
+    // navbar: navbarReducer,
+    bestSeller: bestseller
+}
+
+const store = configureStore({
+  reducer: reducer,
+  devTools: true,
+});
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>
+
+export default store;
---
+++ src/store/slices/bestseller.ts
@@ -0,0 +1,40 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { RootState } from '..';
+const bsetSellerSlice = createSlice({
+    name: 'bestSeller',
+    initialState: { 
+        data: [
+            {
+                id:'00',
+                number: '123456',
+                qty: 10
+            },
+            {
+                id:'00',
+                number: '876356',
+                qty: 5
+            },
+            {
+                id:'00',
+                number: '176826',
+                qty: 8
+            },
+            {
+                id:'00',
+                number: '993456',
+                qty: 9
+            },
+            {
+                id:'00',
+                number: '244456',
+                qty: 1
+            }
+        ]
+    },
+    reducers: { 
+
+    }
+});
+
+export const bsetSeller = (state: RootState) => state.bestSeller;
+export default bsetSellerSlice.reducer;
