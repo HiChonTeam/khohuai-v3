8e1661062a64aa140c0d9f2a8db4187b28725634
chon26909 <59905927+chon26909@users.noreply.github.com>
2022-04-17 10:39:23 +0700

    update credential

--- src/api/http.ts
+++ src/api/http.ts
@@ -4,7 +4,4 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://khohuai-v3-api.herokuapp.com",
   withCredentials: true,
-  headers: {
-    "Content-type": "application/json"
-  }
 });
